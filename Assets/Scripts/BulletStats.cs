44c24e9600e1ec6b32a1070106080104852e2998
xandersalamander <xanderthehighlandersalamander@gmail.com>
2024-05-08 23:45:23 +0900

    feat: add bullet stats

---
+++ Assets/Scripts/BulletStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStats : MonoBehaviour
+{
+    // Stats
+    public float speed = 10;
+    public float destroyAfterSeconds = 3;
+}
--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -6,15 +6,18 @@ using TMPro;
 
 public class GunManager : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 5;
-    public float destroyAfterSeconds = 5;
+    public GameObject bullet;
     public Transform bulletSpawnPoint;
 
     [SerializeField] private TextMeshProUGUI debugScreenText;
     private bool canShootLeft = false;
     private bool canShootRight = false;
+    private BulletStats bulletStats;
 
+    void Start()
+    {
+        bulletStats = bullet.GetComponent<BulletStats>();
+    }
 
     void OnTriggerEnter(Collider collider)
     {
@@ -79,13 +82,13 @@ public class GunManager : MonoBehaviour
     // Fire bullet
     void FireBullet()
     {
-        GameObject spawnedBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        GameObject spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
         Rigidbody spawnedBulletRB = spawnedBullet.GetComponent<Rigidbody>();
         // Place it in bulletSpawnPoint and give it initial velocity
         spawnedBullet.transform.position = bulletSpawnPoint.position;
-        spawnedBulletRB.velocity = bulletSpawnPoint.transform.forward * bulletSpeed;
+        spawnedBulletRB.velocity = bulletSpawnPoint.transform.forward * bulletStats.speed;
         // Destroy the bullet
-        Destroy(spawnedBullet, destroyAfterSeconds);
+        Destroy(spawnedBullet, bulletStats.destroyAfterSeconds);
     }
 
     public void debug()
@@ -93,8 +96,13 @@ public class GunManager : MonoBehaviour
         if (debugScreenText != null)
         {
             string debuggingText = "Debug:";
+            debuggingText += "\n";
             debuggingText += "\ncanShootLeft = " + canShootLeft.ToString();
             debuggingText += "\ncanShootRight = " + canShootRight.ToString();
+            debuggingText += "\n";
+            debuggingText += "\nBullet Stats:";
+            debuggingText += "\nBullet Speed = " + bulletStats.speed.ToString();
+            debuggingText += "\nBullet Duration = " + bulletStats.destroyAfterSeconds.ToString() + "s";
             debugScreenText.text = debuggingText;
         }
     }
