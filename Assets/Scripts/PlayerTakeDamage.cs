754374debc69c587d702b5786d2aa6bebe176ae7
xandersalamander <xanderthehighlandersalamander@gmail.com>
2024-06-30 13:34:36 +0900

    chore: update script tooltips

--- Assets/Scripts/PlayerTakeDamage.cs
+++ Assets/Scripts/PlayerTakeDamage.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerTakeDamage : MonoBehaviour
 {
     [SerializeField] PlayerHealth playerHealth;
+    [Tooltip("Reference to player Health script")]
     [SerializeField] float damage_multiplier = 1.0f;
-    [SerializeField] string bulletTag = "Bullet";
+    [Tooltip("Damage multiplier for this collider")]
+    [SerializeField] string bulletTag = "EnemyBullet";
+    [Tooltip("Tag of objects inflicting damage")]
     [SerializeField] AudioClip hitSound;
+    [Tooltip("Sound to be played when the player is hit")]
     [SerializeField] AudioSource audioSource;
+    [Tooltip("Audio source for the sound")]
     [SerializeField] Animator animator;
+    [Tooltip("Player animator")]
     private bool isHit;
 
     void OnTriggerEnter(Collider collider)
